fa362fdc9b2bcaa3ddd6652a82dc3d9ab50defc2
python <vpnikitos@gmail.com>
2023-03-31 15:59:08 +0300

    1.3

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.vkinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText searchField;
+    private Button searchButton;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchField = findViewById(R.id.et_search_field);
+        searchButton = findViewById(R.id.b_search_vk);
+        result = findViewById(R.id.tv_result);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("Кнопка была нажата");
+            }
+        };
+
+        searchButton.setOnClickListener(onClickListener);
     }
 }
